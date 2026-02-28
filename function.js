38ce733dafc72388f7990c2d27e3c1233946e33e
tushank2753 <tushankilu2753@gmail.com>
2026-02-28 10:15:45 +0530

    Updated bootstrap assignment

--- function.js
+++ function.js
@@ -1,18 +1,18 @@
-// Function Definition
-debugger
-function introduceMe(username, profession, age) {
-    console.log(typeof username);
-    console.log(typeof profession);
-    console.log(typeof age);
-    console.log('Hi,');
-    console.log(`My name is ${username || 'Procodrr'}.`);
-    console.log(`I am a ${profession}`);
-    console.log(`I am ${age} years old.`); 
-}
-introduceMe()
+// // Function Definition
+// debugger
+// function introduceMe(username, profession, age) {
+//     console.log(typeof username);
+//     console.log(typeof profession);
+//     console.log(typeof age);
+//     console.log('Hi,');
+//     console.log(`My name is ${username || 'Procodrr'}.`);
+//     console.log(`I am a ${profession}`);
+//     console.log(`I am ${age} years old.`);
+// }
+// introduceMe()
 
 
-// Function Call
+// // // Function Call
 // const returnValue = introduceMe()
 // introduceMe('Anurag', 'Software Engineer', 25)
 // introduceMe('Akash', 'Mechanical Engineer', 27)
@@ -25,3 +25,30 @@ introduceMe()
 // return 0
 // }
 // introduceMe()
+
+
+// debugger
+// const username = 'Anurag'
+// const userAge = 25
+// sayHi()
+// function sayHi() {
+//     const a = 14
+//     const b = 12
+//     // return a + b
+//     add(7, 9)
+// }
+// function add(x, y) {
+//     kuchhBhi()
+//     return x + y
+// }
+// function kuchhBhi() {
+//     console.log('Kuchh bhi');
+// }
+
+// debugger
+// function introduceMe() {
+//     console.log('Hi, My name.is.Anurag.');
+//     introduceMe()
+// }
+// introduceMe()
+// console.log('Program Ended');
