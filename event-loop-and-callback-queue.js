4e75470ea3914bea473b23c3c0e70f8dcd9e5a0b
tushank2753 <tushankilu2753@gmail.com>
2026-03-15 20:27:11 +0530

    Event Loop and Callback Queue

---
+++ event-loop-and-callback-queue.js
@@ -0,0 +1,37 @@
+// debugger
+// // EXAMPLE :- Event Loop + Callback Queue (setTimeout)
+// console.log('Hi-1');
+// function hello() {
+//     console.log('Hello World!');
+// }
+// for(let i = 1; i <=4; i++){
+//     console.log(i);
+// }
+// hello()
+// setTimeout(hello, 0)
+// console.log('Hi-2')
+
+
+// // EXAMPLE :- Event Loop + Callback Queue (setTimeout)
+// console.log("Start");
+// function greet() {
+//   console.log("Hello from greet");
+// }
+// setTimeout(greet, 0);
+// console.log("End");
+
+
+// // EXAMPLE :-  Call Stack Example
+// console.log('Hello');
+// function fn1() {
+//     console.log("Hello from fn1");
+// }
+// function fn2() {
+//     console.log("Hello from fn2");
+// }
+// function fn3() {
+//     fn2();
+//     console.log("Hello from fn3")
+// }
+// fn3();
+// fn1();
