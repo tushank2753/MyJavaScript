ca7d00f9fa17cc859bbadda453ad983ee3d87e58
tushank2753 <tushankilu2753@gmail.com>
2026-03-30 12:55:55 +0530

    Multi Dimensional Array

---
+++ muti-D-array.js
@@ -0,0 +1,23 @@
+// let matrix = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ];
+
+// for (let i = 0; i < matrix.length; i++) {
+//   for (let j = 0; j < matrix[i].length; j++) {
+//     console.log(matrix[i][j]);
+//   }
+// }
+
+// let marks = [
+//   ["Aman", 85, 90],
+//   ["Riya", 78, 88],
+//   ["John", 92, 95]
+// ];
+
+// console.log(marks[1][0]); // Riya
+// console.log(marks[1][2]); // 88
+
+
+// 32.
