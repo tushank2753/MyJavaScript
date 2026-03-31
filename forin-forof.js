a1515b97a621cd4a2a5b29763b513adc7b8bd49c
tushank2753 <tushankilu2753@gmail.com>
2026-03-31 13:01:21 +0530

    Example of for in and for of loop

---
+++ forin-forof.js
@@ -0,0 +1,12 @@
+debugger
+// const arr = [10, 20, 30];
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+// const str = "hello";
+
+// for (let ch of str) {
+//   console.log(ch);
+// }
