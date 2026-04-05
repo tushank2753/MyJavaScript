777c4f6546a7ce6fe4d03b4fe4714aa783394710
tushank2753 <tushankilu2753@gmail.com>
2026-04-05 16:33:20 +0530

    Explain forEach-Array

---
+++ forEach-array.js
@@ -0,0 +1,18 @@
+debugger
+// const Fruits = ['Apple', 'Banana', 'Orange', 'Grapes']
+// const upperCase = Fruits.map(function(Fruits){
+//     console.log(Fruits.toUpperCase())
+//     return Fruits.toUpperCase()
+// })
+// console.log(upperCase)
+
+
+// const numbers = [1,2,3,4,5]
+// const storevar = numbers.forEach(function(num){
+//     console.log(num)
+// })
+// // if numbers.forEach(function(num){
+//     // console.log(num)
+// // })
+// // ko ek variable ke under store karane pr us variable ko console krte hai to humko return me undefined milega
+// console.log(storevar)
