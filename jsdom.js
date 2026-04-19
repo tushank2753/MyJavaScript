913a8f7c67e19743281917fd908003418c0d7ed0
tushank2753 <tushankilu2753@gmail.com>
2026-04-20 00:00:00 +0530

    in this DOM manipulation

---
+++ jsdom.js
@@ -0,0 +1,30 @@
+// const allImages = document.images
+// const allImages = document.getElementsByTagName('img')
+// const cssImage = document.getElementsByClassName('css-image')[0]
+// const cssImage = document.getElementById('css-image')
+// const cssImage = document.querySelectorAll('#css-image')
+// const cssImage = document.querySelector('.css-image')
+// const jsImage = document.querySelector('[sameer="software"]');
+// const ul = document.querySelector('ul')
+
+// const allImages = document.querySelectorAll('img')
+
+// // const imageInsideUl = ul.querySelector('.css-image')
+
+// const imagesUrl = [
+//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2WiZeZxHy5UwINXjrKPDC0AqUuBkNBIU8mQ&s',
+//   'https://www.propertyfinder.ae/blog/wp-content/uploads/2024/09/All-About-IMG-Worlds-of-Adventure1.jpg',
+//   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8AcRC1bv-NCjZaHXI8aSaLDzq8kvT-h_8iw&s',
+// ]
+
+// allImages[0].src = imagesUrl[0]
+// allImages[1].src = imagesUrl[1]
+// allImages[2].src = imagesUrl[2]
+
+// for (let i = 0; i < allImages.length; i++) {
+//   allImages[i].src = imagesUrl[i]
+// }
+
+// allImages.forEach((image, i) => {
+//   image.src = imagesUrl[i]
+// })
