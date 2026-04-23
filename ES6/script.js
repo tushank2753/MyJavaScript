0d916cc75fd932540a7559f6b1f118b004935b93
tushank2753 <tushankilu2753@gmail.com>
2026-04-23 13:38:15 +0530

    Import and Export data from another Files

---
+++ ES6/script.js
@@ -0,0 +1,29 @@
+// import { userData } from "./userdata.js";
+// import { productData } from "./products.js";
+// console.log(userData[0].firstName)
+// console.log(productData)
+
+// import kuchbhi from "./userdata.js";
+// console.log(kuchbhi)
+// import kuchbhi2 from "./products.js"
+// console.log(kuchbhi2)
+
+
+// import {myName} from "./userdata.js";
+// console.log(myName)
+// import { myProduct } from "./products.js";
+// console.log(myProduct)
+
+
+// import {myName as Name} from "./userdata.js";
+// console.log(Name)
+// import {myProduct as Product} from "./products.js";
+// console.log(Product)
+
+
+// import { userData } from "./userdata.js";
+// import kuchbhi from "./userdata.js";
+// console.log(kuchbhi)
+
+import * as data from "./userData.js";
+console.log(data.userData);
