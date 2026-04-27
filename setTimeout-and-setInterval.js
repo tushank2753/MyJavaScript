09fc08015f565e32fc32618ed1b329af9a22ca2c
tushank2753 <tushankilu2753@gmail.com>
2026-04-27 15:19:14 +0530

    Set-Timeout and Set-Interval

---
+++ setTimeout-and-setInterval.js
@@ -0,0 +1,11 @@
+// debugger
+// console.log("Start");
+// setTimeout(function () { 
+//     console.log("Time up") 
+// }, 3000);
+// console.log("End");
+
+// function greet(name) {
+//   console.log("Hello " + name)
+// }
+// setTimeout(greet, 2000, "Rahul")
