25e8d1efeee0625c310a9600b0de93bb6d43a8bf
tushank2753 <tushankilu2753@gmail.com>
2026-05-01 10:12:28 +0530

    for each html

--- forEach-array.js
+++ forEach-array.js
@@ -7,12 +7,12 @@ debugger
 // console.log(upperCase)
 
 
-// const numbers = [1,2,3,4,5]
-// const storevar = numbers.forEach(function(num){
-//     console.log(num)
+const numbers = [1,2,3,4,5]
+const storevar = numbers.forEach(function(num){
+    console.log(num)
+})
+// if numbers.forEach(function(num){
+    // console.log(num)
 // })
-// // if numbers.forEach(function(num){
-//     // console.log(num)
-// // })
 // // ko ek variable ke under store karane pr us variable ko console krte hai to humko return me undefined milega
 // console.log(storevar)
