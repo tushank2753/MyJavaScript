7e4c729121a35691a12d9437741d3d7024632e11
tushank2753 <tushankilu2753@gmail.com>
2026-05-04 23:21:48 +0530

    styling in javascript how to style in js

--- style-in-js.js
+++ style-in-js.js
@@ -4,32 +4,32 @@ const allLinks = document.querySelectorAll('a')
 const imglink = document.getElementById('css-image')
 
 
-// console.log(paragraph.innerHTML);
-
-// paragraph.innerHTML = '<h4>Hiii</h4>'
+console.log(paragraph.innerHTML);
+// 
+paragraph.innerHTML = '<h4>Hiii</h4>'
 
 h1.style.color = 'hotpink'
 h1.style.backgroundColor = 'skyblue'
 
-// for (let i = 0; i < allLinks.length; i++) {
-//   allLinks[i].style.color = 'teal'
-// }
+for (let i = 0; i < allLinks.length; i++) {
+  allLinks[i].style.color = 'teal'
+}
 
 
 for (const link of allLinks) {
-    // link.style.color = 'teal'
-    // link.style.textDecorationLine = 'none'
-    // link.style.fontWeight = '700'
-    // link.style.fontFamily = 'cursive'
-    // link.style.fontSize = '18px'
+    link.style.color = 'teal'
+    link.style.textDecorationLine = 'none'
+    link.style.fontWeight = '700'
+    link.style.fontFamily = 'cursive'
+    link.style.fontSize = '18px'
 
-    // link.style.cssText = `
-    //     color: teal;
-    //     text-decoration-line: none;
-    //     font-weight: 700;
-    //     font-family: cursive;
-    //     font-size: 18px;
-    // `
+    link.style.cssText = `
+        color: teal;
+        text-decoration-line: none;
+        font-weight: 700;
+        font-family: cursive;
+        font-size: 18px;
+    `
 
     link.className = 'green-link'
     // link.setAttribute('class', 'green-link')
